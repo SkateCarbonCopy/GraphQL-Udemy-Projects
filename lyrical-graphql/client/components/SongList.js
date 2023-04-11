caf21efd219c835968f8b62af4991089c4f9e695
SkateCarbonCopy <crgastelle@gmail.com>
2023-04-11 16:58:48 -0600

    finishing Section 6: Clientside GraphQL

---
+++ lyrical-graphql/client/components/SongList.js
@@ -0,0 +1,26 @@
+import { defaultWebIncludes } from 'babel-preset-env/lib/default-includes';
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class SongList extends Component {
+    render() {
+        console.log(this.props)
+
+        return (
+            <div>
+                SongList
+            </div>
+        )
+    }
+}
+
+const query = gql`
+    {
+        songs {
+            title
+        }
+    }
+`;
+
+export default graphql(query)(SongList);
